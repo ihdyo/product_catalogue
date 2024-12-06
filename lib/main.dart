a9c2126f02ed07cc220fe2d9616db3dfc3eecbb8
ihdyo <yodhianugrah23@gmail.com>
2024-12-06 15:39:10 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: ,
+    );
   }
 }
